b1a46e6bc1cfbb880cb896c7c4b45e3db5a2a3c7
Judas <Jonas1999@live.se>
2018-09-19 22:55:10 +0200

    made main code

--- MiniWig/MiniWig/main.cpp
+++ MiniWig/MiniWig/main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
 	int boolaid = 1;
+	int col = 0;
 
 	while (boolaid > 0) {
+		col++;
+		system("cls");
+
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(hConsole, col);
+		//boolaid--;
+		if (col == 255) {
+			col = 1;
+		}
+		Beep(523, 5); // 523 hertz (C5) for 5 milliseconds
+
+
+
+		cout << "    __  ___   _             _    _       __   _         " << endl
+			<< "   /  |/  /  (_)  ____     (_)  | |     / /  (_)  ____ _" << endl
+			<< "  / /|_/ /  / /  / __ \   / /   | | /| / /  / /  / __ `/" << endl
+			<< " / /  / /  / /  / / / /  / /    | |/ |/ /  / /  / /_/ / " << endl
+			<< "/_/  /_/  /_/  /_/ /_/  /_/     |__/|__/  /_/   \__, /  " << endl
+			<< "                                               /____/   " << endl;
+
+		
 
 	}
 
-
+	system("pause");
 	return 0;
 }
